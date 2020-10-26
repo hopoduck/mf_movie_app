e1bbe76f4ec41f633d2daae57552372cd6cb5b80
hopoduck <gksqudwh1@naver.com>
2020-10-26 17:19:23 +0900

    3.0 Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-class App extends React.Component {}
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    );
+  }
+}
 
 export default App;
